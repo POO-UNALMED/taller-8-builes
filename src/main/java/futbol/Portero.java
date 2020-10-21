b55b3e7e685417a11ca75caa880e0ca43f9b5fac
builes <70285622+builes@users.noreply.github.com>
2020-10-20 22:02:09 -0500

    Update Portero.java

--- src/main/java/futbol/Portero.java
+++ src/main/java/futbol/Portero.java
@@ -3,10 +3,10 @@ package futbol;
 public class Portero extends Jugador {
 	public short golesRecibidos;	
 	
-	public Portero(String nombre, int edad, short golesMarcados, byte dorsal) {
-		super(nombre, edad, "Portero", golesMarcados, dorsal);
+	public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
+		super(nombre, edad, "Portero", (short)0, dorsal);
 		this.golesRecibidos = golesRecibidos;	
-	}
+	}	
 	
 	public int compareTo(Portero f) {
 		return this.golesRecibidos - f.golesRecibidos;
